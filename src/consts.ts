6dc894156c172c4d72903d38ef21a63361acdd99
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2024-11-17 18:08:21 -0300

    News updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'David Silvera - Data Scientist & AI Developer';
+export const SITE_DESCRIPTION = 'Welcome to my website! I am a data scientist and AI developer with a passion for building innovative solutions to complex problems.';
